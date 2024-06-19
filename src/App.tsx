83e39f7bc9ed788647e2c538f20ed8819a56fed5
MariaJoseGG <mgiraldogu@unal.edu.co>
2024-06-19 15:33:03 -0500

    fix: the name of the country is taken from the environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <Header />
       <div className="flex flex-col gap-5 p-6">
         <h1 className="text-center text-2xl text-dodger-blue-950 font-bold">Calidad del Aire en {import.meta.env.VITE_COUNTRY}</h1>
-        <p>A continuación, podrá consultar el clima y los datos de calidad del aire actuales en Colombia.</p>
+        <p>A continuación, podrá consultar el clima y los datos de calidad del aire actuales en {import.meta.env.VITE_COUNTRY}.</p>
         <CityForm />
       </div>
     </div>
